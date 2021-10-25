3bcf7777db0bb9ed27f79ff86d34c5c135aec891
Anthony Wlodarczyk <amw6765@psu.edu>
2021-10-25 14:42:13 -0400

    JavaScriptExercise 2 and 3 bug fixes and additions to js and html file

--- docs/AMW_JavaScript_Exercise1.js
+++ docs/AMW_JavaScript_Exercise1.js
@@ -3,11 +3,18 @@ window.addEventListener('DOMContentLoaded',tiger,false);
 function tiger() {
     alert ('Hi there! Looks like the page loaded! Yay!');
     var buttons = document.getElementsByTagName("button")
-buttons[0].addEventListener('click', changeColor,false)
-
+buttons[0].addEventListener('click', dateTime ,false)
+buttons[1].addEventListener('click', changeColor, false)
 }
 
+function dateTime() {
+    document.getElementById('dateTime').innerHTML = Date();
+    
+}
 
 function changeColor() {
-var p1 = document.getElementById("colorToggle") 
-{p1.style.backgroundColor = "skyblue";}
+var p1 = document.getElementById("welcome") 
+console.log('This function is firing!')
+{p1.style.backgroundColor = "aqua";}
+}
+
